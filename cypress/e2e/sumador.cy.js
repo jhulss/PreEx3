9772af3f90a8ccd4473e2c98056790128d61590e
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-06-16 11:45:47 -0400

    Se puede publicar un nuevo post con titulo y descripcion

--- cypress/e2e/sumador.cy.js
+++ cypress/e2e/sumador.cy.js
@@ -1,9 +1,29 @@
-describe("Sumador", () => {
-  it("Shows the amount of the addition to the user", () => {
+describe("Publicar un nuevo post", () => {
+  it("Muestra un post", () => {
+    const titulo = "Título del post";
+    const detalle = "Contenido detallado del post";
     cy.visit("/");
-    cy.get("#primer-numero").type(4);
-    cy.get("#segundo-numero").type(5);
-    cy.get("#sumar-button").click();
-    cy.get("#resultado-div").should("contain", "9");
+    cy.get("#titulo").type(titulo);
+    cy.get("#descripcion").type(detalle);
+    cy.get("#postForm").submit();
+    cy.get("#resultado-div").should("contain", titulo);
+    cy.get("#resultado-div").should("contain", detalle);
   });
+
+  it("No permite que el titulo exceda los 50 caracteres", () => {
+    const titulo = "lkjhdfljk aaflkjhsa ldfalskd jhfasjdhf alskdjhf asdkjfha sdjhflaksd jfhk jsdflka s";
+    cy.visit("/");
+    cy.get("#titulo").type(titulo);
+    cy.get("#postForm").submit();
+    cy.get("#resultado-div").should("contain", "El título excede la longitud máxima permitida"); 
+  });
+
+  it("No deberia permitir que el detalle sobrepase los 150 caracteres", () => {
+    const detalle = "Contenido detallado del post es largo para que falle el post ya que tiene que fallar fija lija la pija esta mierda que no quiere pasar pero tiene que pasar algun rato porque si y para que yo pueda aprender bien";
+    cy.visit("/");
+    cy.get("#descripcion").type(detalle);
+    cy.get("#postForm").submit();
+    cy.get("#resultado-div").should("contain", "El detalle excede la longitud máxima permitida"); 
+  });
+
 });
