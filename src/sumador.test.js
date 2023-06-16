70a2f622780a1b61cb4475c62a0e650521dff8f9
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-06-16 11:48:24 -0400

    No de permite que el titulo sobrepase los 50 caracteres

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -10,13 +10,21 @@ describe("Postear", () => {
       { titulo: titulo, detalle: detalle }
     ]);
   });
-  
+
   it("No deberia permitir que un post se publique sin titulo", () => {
     const postear = new Post();
     const titulo = "";
     const detalle = "Contenido detallado del post";
     const resultado = postear.publicarPost(titulo, detalle);
     expect(resultado).toEqual("No es posible publicar un post sin título");
+  });
+
+  it("No deberia permitir que el titulo sobrepase los 50 caracteres", () => {
+    const postear = new Post();
+    const titulo = "lkjhdfljk aaflkjhsa ldfalskd jhfasjdhf alskdjhf asdkjfha sdjhflaksd jfhk jsdflka s";
+    const detalle = "Contenido detallado del post";
+    const resultado = postear.publicarPost(titulo, detalle);
+    expect(resultado).toEqual("El título excede la longitud máxima permitida");
   });
 
   
