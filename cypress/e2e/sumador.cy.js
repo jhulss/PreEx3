0312e47423b802fe72fd355773bc94e82821267a
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-06-16 11:50:23 -0400

    publicar un nuevo post - cypress

--- cypress/e2e/sumador.cy.js
+++ cypress/e2e/sumador.cy.js
@@ -10,20 +10,8 @@ describe("Publicar un nuevo post", () => {
     cy.get("#resultado-div").should("contain", detalle);
   });
 
-  it("No permite que el titulo exceda los 50 caracteres", () => {
-    const titulo = "lkjhdfljk aaflkjhsa ldfalskd jhfasjdhf alskdjhf asdkjfha sdjhflaksd jfhk jsdflka s";
-    cy.visit("/");
-    cy.get("#titulo").type(titulo);
-    cy.get("#postForm").submit();
-    cy.get("#resultado-div").should("contain", "El título excede la longitud máxima permitida"); 
-  });
 
-  it("No deberia permitir que el detalle sobrepase los 150 caracteres", () => {
-    const detalle = "Contenido detallado del post es largo para que falle el post ya que tiene que fallar fija lija la pija esta mierda que no quiere pasar pero tiene que pasar algun rato porque si y para que yo pueda aprender bien";
-    cy.visit("/");
-    cy.get("#descripcion").type(detalle);
-    cy.get("#postForm").submit();
-    cy.get("#resultado-div").should("contain", "El detalle excede la longitud máxima permitida"); 
-  });
+
+  
 
 });
