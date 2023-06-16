8e4ad3a87621253aa55a5b746a9368a84873ba16
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-06-16 11:47:09 -0400

    No de permite que se postee un post sin titulo

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -10,6 +10,14 @@ describe("Postear", () => {
       { titulo: titulo, detalle: detalle }
     ]);
   });
+  
+  it("No deberia permitir que un post se publique sin titulo", () => {
+    const postear = new Post();
+    const titulo = "";
+    const detalle = "Contenido detallado del post";
+    const resultado = postear.publicarPost(titulo, detalle);
+    expect(resultado).toEqual("No es posible publicar un post sin título");
+  });
 
   
 });
